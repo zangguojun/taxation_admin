ea93d4e82fa6d59b0a99cecea799d6f8c1769a9f
zangguojun <1739932260@qq.com>
2021-01-29 21:30:07 +0800

    更改目录结构

--- taxation/taxation/settings/dev.py
+++ taxation/taxation/settings/dev.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -117,11 +119,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+# 设置自定义的用户模型类
+AUTH_USER_MODEL = 'users.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Redis缓存配置
 CACHES = {
