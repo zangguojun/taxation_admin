b26041ec02ae4925cb22cf1ee47bf463c63a1a71
zangguojun <1739932260@qq.com>
2021-01-29 18:52:06 +0800

    start

---
+++ taxation/utils/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class BaseModel(models.Model):
+    """为模型类补充字段"""
+
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
